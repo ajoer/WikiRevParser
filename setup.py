c59bf7b6df1ad21e3e34dd697a55275647fab774
Anna Jørgensen <anka.jorgensen@gmail.com>
2020-03-20 16:06:43 +0100

    updating license

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="wikirevparser", # Replace with your own username
+    name="wikirevparser",
     version="0.0.1",
     author="Anna Jørgensen",
     author_email="anka.jorgensen@gmail.com",
     description="Wikipedia revision history parser for Python",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/ajoer/WikiRevisionParser",
     packages=setuptools.find_packages(),
     classifiers=[
